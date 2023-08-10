c532f1e190779e4e2d5f6265f630bb455d0b9614
Tr1st0n <tristonxmoulton@hotmail.com>
2023-08-10 18:28:34 -0400

    Hamburger menu added

--- script.js
+++ script.js
@@ -263,3 +263,7 @@ class TextScramble {
     }
   } 
 
+  function show() {
+    document.querySelector('.ham').classList.toggle('open')
+    document.querySelector('.hamburger').classList.toggle('active')
+}
